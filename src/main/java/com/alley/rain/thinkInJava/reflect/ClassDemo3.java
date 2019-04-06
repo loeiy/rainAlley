8c1c67c8bdb45cd8780540ec1c90452df86cca51
loeiy <looeiy@foxmail.com>
2019-04-06 15:14:12 +0800

    tes

--- src/main/java/com/alley/rain/thinkInJava/reflect/ClassDemo3.java
+++ src/main/java/com/alley/rain/thinkInJava/reflect/ClassDemo3.java
@@ -3,7 +3,7 @@ package com.alley.rain.thinkInJava.reflect;
 public class ClassDemo3 {
     public static void main(String[] args) {
         String s = "adfs";
-        Integer i = 11;
+        Integer i = 12;
         ClassUtil.printClassMethodMessage(i);
         ClassUtil.printClassFieldMessage(i);
         ClassUtil.printClassConstructor(i);
