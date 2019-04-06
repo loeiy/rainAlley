02dc890bc5942e98e152e079476cd1c6cf53502c
loeiy <looeiy@foxmail.com>
2019-04-06 15:26:43 +0800

    reflectDemo

---
+++ src/main/java/com/alley/rain/thinkInJava/reflect/ReflectDemo.java
@@ -0,0 +1,14 @@
+package com.alley.rain.thinkInJava.reflect;
+
+/**
+ * Description: 反射使用类Demo
+ * User: dyf
+ * Date: 2019-04-06
+ * Time: 15:25
+ */
+public class ReflectDemo {
+
+    public static void main(String[] args) {
+        System.out.println("dsfs");
+    }
+}
